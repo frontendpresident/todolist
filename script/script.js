21825ad2097c5d17478b279a25882f01b1c7b91f
Sergey Maifetov <smaifetov.exceedteam@gmail.com>
2021-06-11 10:05:34 +0300

    fix

--- script/script.js
+++ script/script.js
@@ -10,32 +10,44 @@ let allTasks = document.querySelector('.allTasks')
 let arrayOfMessages = []
 
 const displayMessages = () => {
+
     list.innerHTML = ''
 
-    arrayOfMessages.forEach(item => {
+    arrayOfMessages.forEach((item, index) => {
         const wrapper = document.createElement('div')
         wrapper.className = 'wrapperMessages'
+        const btnDeleteTasks = document.createElement('button')
+        btnDeleteTasks.className = 'btnDeleteTasks'
+        btnDeleteTasks.innerHTML = '❌'
 
         const todoTitle = document.createElement('h3')
         todoTitle.innerHTML = item.text
         todoTitle.addEventListener('click', () => {
-            console.log('heheheeh')
+     
+            todoTitle.className = "doneWrapperMessages"
+            item.isDone = true
         })
 
+
         wrapper.appendChild(todoTitle)
-
         list.appendChild(wrapper)
-        // const todo = `<div class="wrapperMessages"><h3>${item.text}</h3> <button class="btnDeleteTasks">❌</button></div>`;
+        wrapper.appendChild(btnDeleteTasks)
+        btnDeleteTasks.onclick = () => {
 
-        // list.innerHTML += todo
+            wrapper.remove()
+            arrayOfMessages.splice(index, index + 1)
+        }
     })
 
 }
 
 addTodoBtn.onclick = () => {
+
     if (input.value === '') {
+
         alert('Please enter todo!')
         return
+
     }
 
     const todo = {
@@ -48,69 +60,143 @@ addTodoBtn.onclick = () => {
 
     input.value = '';
 
-    //Выполненые задачи.
-    doneTasks.onclick = () => {
+}
 
-        list.innerHTML = ''
 
-        let doneTaskEl = ''
-        let filterDoneTask = arrayOfMessages.filter(f => f.status === true); console.log(filterDoneTask)
-        filterDoneTask.forEach(item => {
-            doneTaskEl += `<div class = 'doneWrapperMessages'>
-            <h3 class = "">${item.todo}</h3> 
-            <button class= 'btnDeleteTasks'>❌</button>
-            </div>
-            `;
+doneTasks.onclick = () => {
 
-            list.innerHTML = doneTaskEl;
+    list.innerHTML = ''
+
+    let filterDoneTask = arrayOfMessages.filter(f => f.isDone === true);
+    filterDoneTask.forEach((item, index) => {
+
+        const wrapper = document.createElement('div')
+        wrapper.className = 'wrapperMessages'
+
+        const todoTitle = document.createElement('h3')
+        todoTitle.innerHTML = item.text
+        todoTitle.className = 'doneWrapperMessages'
+
+        todoTitle.addEventListener('click', () => {
+
+            todoTitle.className = ""
+            item.isDone = false;
+         
         })
-    }
 
-    //Не выполненые задачи.
-    notDoneTasks.onclick = () => {
-        list.innerHTML = ''
+        const btnDeleteTasks = document.createElement('button')
+        btnDeleteTasks.className = 'btnDeleteTasks'
+        btnDeleteTasks.innerHTML = '❌'
 
-        let notDoneTaskEl = ''
-        let filterNotDoneTask = arrayOfMessages.filter(f => f.status === false);
-        filterNotDoneTask.forEach(item => {
-            notDoneTaskEl += `<div class = "wrapperMessages">
-            <h3>${item.todo}</h3>
-            <button class= 'btnDeleteTasks'>❌</button>
-            <div/>`;
+        wrapper.appendChild(todoTitle)
+        list.appendChild(wrapper)
 
-            list.innerHTML = notDoneTaskEl;
+        wrapper.appendChild(btnDeleteTasks)
+        btnDeleteTasks.onclick = () => {
 
-        })
-    }
-    //Все задачи.
-    allTasks.onclick = () => {
-
-        list.innerHTML = ''
-
-        let allTasksEl = ''
-        arrayOfMessages.forEach(item => {
-            allTasksEl += `<div class = "${toDoList.status === true ? 'doneWrapperMessages' : 'wrapperMessages'}">
-            <h3>${item.todo}</h3>
-            <button class= 'btnDeleteTasks'>❌</button>
-            </div>`;
-            list.innerHTML = allTasksEl;
-
-        })
-    }
-
-    //Удалить задачу.
+            wrapper.remove()
+            arrayOfMessages.splice(index, index + 1)
+        }
+    })
 
 
 }
 
+notDoneTasks.onclick = () => {
+
+    list.innerHTML = ''
+
+    let filterNotDoneTask = arrayOfMessages.filter(f => f.isDone !== true);
+
+    filterNotDoneTask.forEach((item, index) => {
+
+        const wrapper = document.createElement('div')
+        wrapper.className = 'wrapperMessages'
+
+        const todoTitle = document.createElement('h3')
+        todoTitle.innerHTML = item.text
+
+        todoTitle.addEventListener('click', () => {
+
+            if(item.isDone !== true){
+                todoTitle.className = 'doneWrapperMessages'
+                return
+            }
+            todoTitle.className = ''
+            item.isDone = false
+        })
+
+        const btnDeleteTasks = document.createElement('button')
+        btnDeleteTasks.className = 'btnDeleteTasks'
+        btnDeleteTasks.innerHTML = '❌'
+
+        wrapper.appendChild(todoTitle)
+        list.appendChild(wrapper)
+
+        wrapper.appendChild(btnDeleteTasks)
+        btnDeleteTasks.onclick = () => {
+
+            wrapper.remove()
+            arrayOfMessages.splice(index, index + 1)
+
+        }
+
+    })
 
 
+}
 
-//Очистить все.
+allTasks.onclick = () => {
+
+    list.innerHTML = ''
+
+    arrayOfMessages.forEach((item, index) => {
+        const wrapper = document.createElement('div')
+        wrapper.className = 'wrapperMessages'
+
+        const todoTitle = document.createElement('h3')
+        todoTitle.innerHTML = item.text
+
+        todoTitle.addEventListener('click', () => {
+
+            if(item.isDone !== true){
+                todoTitle.className = 'doneWrapperMessages'
+                return
+            }
+            todoTitle.className = ''
+            item.isDone = false
+        })
+
+        const btnDeleteTasks = document.createElement('button')
+        btnDeleteTasks.className = 'btnDeleteTasks'
+        btnDeleteTasks.innerHTML = '❌'
+
+        if (item.isDone === true) {
+            todoTitle.className = 'doneWrapperMessages'
+        }
+
+        wrapper.appendChild(todoTitle)
+        list.appendChild(wrapper)
+
+        wrapper.appendChild(btnDeleteTasks)
+        btnDeleteTasks.onclick = () => {
+
+            wrapper.remove()
+            arrayOfMessages.splice(index, index + 1)
+
+        }
+
+    })
+    
+}
+
+
 clearListBtn.onclick = () => {
     while (arrayOfMessages.length > 0) {
+
         arrayOfMessages.pop()
     }
+
     list.innerHTML = ''
 }
 
